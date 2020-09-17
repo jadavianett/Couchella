c40d0fe8bc2bbd95e6c8e838264c8193b694bcfe
7J647 <007J647@gmail.com>
2020-09-17 12:04:45 -0400

    import jQuery CDN, set up API call with tastedive and tested

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,48 @@
-console.log("Hello World");
+// console.log("Hello World");
+
+$(document).ready(function () {
+
+// var corsAnywhere = "https://cors-anywhere.herokuapp.com/"
+// $.ajax({
+//     url: corsAnywhere + "https://tastedive.com/api/similar?q=red+hot+chili+peppers%2C+pulp+fiction",
+//     method: "GET"
+//   }).then(function(response) {
+//     console.log(response);
+//   });
+// })
+
+// $("#CityName").text(cityName);
+// var artistName =
+
+// var corsAnywhere = "https://cors-anywhere.herokuapp.com/"
+
+// $.ajax({
+//     url: corsAnywhere + "https://tastedive.com/api/similar?q=" + artistName,
+//     method: "GET"
+//   }).then(function(response) {
+//     console.log(response);
+//     console.log(response.Similar.Results[0].Name);
+//     // console.log(response.similar.results);
+//     // console.log(response.similar.results[2]);
+//   });
+// })
+
+
+
+var corsAnywhere = "https://cors-anywhere.herokuapp.com/"
+$.ajax({
+    url: corsAnywhere + "https://tastedive.com/api/similar?q=childish+gambino",
+    method: "GET"
+  }).then(function(response) {
+    for (i=0; i<=5; i++) {
+        console.log(response);
+    console.log(response.Similar.Results[i].Name);
+    }
+    // console.log(response.similar.results);
+    // console.log(response.similar.results[2]);
+  });
+})
+
+
+
+
